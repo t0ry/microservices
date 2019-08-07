49646d9836f414582c4f12df33ad43d4ee6fc4db
t0ry <de.berny@gmail.com>
2019-08-07 12:34:48 +0300

    sleuth

--- curency-exchange-service/src/main/java/com/example/ms/curencyexchangeservice/CurrencyExchangeController.java
+++ curency-exchange-service/src/main/java/com/example/ms/curencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.example.ms.curencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,18 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	private final Logger LOGGER = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
 	@Autowired
 	private Environment env;
-	
-	
+
 	@Autowired
 	private ExchangeValueRepository repository;
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue getExchangeValue(@PathVariable String from, @PathVariable String to) {
 		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
-			exchangeValue.setPort(Integer.valueOf(env.getProperty("local.server.port")));
+		exchangeValue.setPort(Integer.valueOf(env.getProperty("local.server.port")));
+
+		LOGGER.info("{}", exchangeValue);
 		return exchangeValue;
 	}
 
--- currency-conversion-service/src/main/java/com/example/ms/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/ms/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +14,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.example.ms.curencyexchangeservice.CurrencyExchangeController;
+
 @RestController
 public class CurrencyConversionController {
-
+	private final Logger LOGGER = LoggerFactory.getLogger(CurrencyConversionController.class);
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private CurrencyExchangeServiceProxy proxy;
 
@@ -39,10 +43,11 @@ public class CurrencyConversionController {
 	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/amount/{amount}")
 	public CurrencyConversionResult convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal amount) {
-		
 
 		CurrencyConversionResult result = proxy.getExchangeValue(from, to);
 		result.setResultAmount(amount.multiply(result.getConversionMultiple()));
+
+		LOGGER.info("{}", result);
 		return result;
 	}
 }
